b5dd5d2567066f83b47fb19ca12860d3f9469df0
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-17 15:41:04 +0300

    Redirect to show article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,17 @@ class ArticlesController < ApplicationController
       @article = Article.new
    end
    def create
-     render plain: params[:article].inspect
+   #   render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+   end
+
+   private 
+   def article_params
+    params.require(:article).permit(:title, :description)
    end
 end
