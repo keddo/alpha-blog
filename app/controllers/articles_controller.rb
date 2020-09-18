fb40f90ee791623ba9a351c3664945d9163f3ed8
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-18 11:45:55 +0300

    Add link to blog articles and about page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,6 +33,13 @@ class ArticlesController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article successfully deleted!"
+    redirect_to articles_path
+  end
    private 
    def article_params
     params.require(:article).permit(:title, :description)
